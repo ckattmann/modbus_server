22ddddf79f5d7a26360f17d657d0cfe90270fbf1
ckattmann <christoph.kattmann@bss-hs.de>
2021-01-04 16:32:30 +0100

    Added basic setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name="simple_modbus_server",
+    version="0.0.1",
+    description="Simple ModbusTCP server library for Python",
+    packages=setuptools.find_packages(),
+)
